4aa0568c294dcc274c77a8dbfbad773fba0c527a
Avery Murray <averylapine@gmail.com>
2021-01-23 13:39:22 -0500

    updgraded PC arguments

--- cimply.c
+++ cimply.c
@@ -12,7 +12,8 @@ extern void *stupid_return(void)
 	return NULL;
 }
 
-extern int cimple_init(struct cimply *_info) {
+extern int cimple_init(struct cimply *_info)
+{
 	_info->pwd = getcwd(stupid_return(), 4096);
 	_info->uid = getuid();
 	_info->euid = geteuid();
@@ -23,35 +24,99 @@ extern int cimple_init(struct cimply *_info) {
 	return 0;
 }
 
+extern int cimple_name(struct cimply *_info)
+{
+	_info->name = getlogin();
+
+	if (_info->name == NULL)
+		return -1;
+	return 0;
+}
+
+extern int cimple_pwd(struct cimply *_info)
+{
+	_info->pwd = getcwd(stupid_return(), 4096);
+
+	if (_info->pwd == NULL)
+		return -1;
+	return 0;
+
+}
+
+extern int cimple_uid(struct cimply *_info)
+{
+	_info->uid = getuid();
+	return 0;
+}
+
+extern int cimple_euid(struct cimply *_info)
+{
+	_info->euid = geteuid();
+	return 0;
+}
+
+extern int cimple_hostname(struct cimply *_info)
+{
+	_info->hostname[1023] = '\0';
+	gethostname(_info->hostname, 1023);
+
+	if (_info->hostname[1] == '\0')
+		return -1;
+	return 0;
+}
 
 // Proton Caller
 
-extern int set_runner_version(struct runner *str, char *arg) {
+extern int set_runner_version(struct runner *str, char *arg)
+{
 	str->version = arg;
 	return 0;
 }
 
-extern int set_runner_path(struct runner *str, char *arg) {
+extern int set_runner_path(struct runner *str, char *arg)
+{
 	str->path = arg;
 	return 0;
 }
 
-extern int set_runner_program(struct runner *str, char *arg) {
+extern int set_runner_program(struct runner *str, char *arg)
+{
 	str->program = arg;
 	return 0;
 }
 
-extern int set_runner_common(struct runner *str, char *arg) {
+extern int set_runner_common(struct runner *str, char *arg)
+{
 	str->common = arg;
 	return 0;
 }
 
-extern int set_runner_arguments(struct runner *str, char *arg) {
+extern int set_runner_arguments(struct runner *str, char *arg)
+{
 	str->arguments = arg;
 	return 0;
 }
 
-extern int set_runner_custom(struct runner *str, int i) {
+extern int set_runner_custom(struct runner *str, int i)
+{
 	str->custom = i;
+	return 0;
+}
+
+extern int set_ar1(struct runner *str, char *arg)
+{
+	str->ar1 = arg;
+	return 0;
+}
+
+extern int set_ar2(struct runner *str, char *arg)
+{
+	str->ar2 = arg;
+	return 0;
+}
+
+extern int set_ar3(struct runner *str, char *arg)
+{
+	str->ar3 = arg;
 	return 0;
 }
--- cimply.h
+++ cimply.h
@@ -4,7 +4,7 @@
 
 #ifndef CIMPLEFETCH_CIMPLY_H
 #define CIMPLEFETCH_CIMPLY_H
-#define CIMPLY_VERSION 0.2
+#define CIMPLY_VERSION 0.3
 
 
 // Cimplefetch
@@ -20,11 +20,19 @@ struct cimply {
 
 };
 
-// Init basic user information into INFO
+// saves given to respective location in INFO
 extern int cimple_init(struct cimply *_info);
 
-// Give INFO HOME and SHELL
-extern int cimply_init_name(struct cimply *_info);
+// fill in specific variable in INFO
+extern int cimple_name(struct cimply *_info);
+
+extern int cimple_pwd(struct cimply *_info);
+
+extern int cimple_uid(struct cimply *_info);
+
+extern int cimple_euid(struct cimply *_info);
+
+extern int cimple_hostname(struct cimply *_info);
 
 
 // Proton Caller
@@ -38,6 +46,10 @@ struct runner {
     char *arguments;
     _Bool custom;
 
+    char *ar1;
+    char *ar2;
+    char *ar3;
+
 };
 
 // saves ARG to respective location in STR
@@ -47,4 +59,8 @@ extern int set_runner_program(struct runner *str, char *arg);
 extern int set_runner_common(struct runner *str, char *arg);
 extern int set_runner_arguments(struct runner *str, char *arg);
 extern int set_runner_custom(struct runner *str, int i);
+
+extern int set_ar1(struct runner *str, char *arg);
+extern int set_ar2(struct runner *str, char *arg);
+extern int set_ar3(struct runner *str, char *arg);
 #endif				//CIMPLEFETCH_CIMPLY_H
